d368f38898cf83f34b30b4137456a82ce3f755e4
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-04-13 04:08:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  family = ["Mom", "Dad", "Cameron", "Christian"]
+    array = ["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  family = ["Mom", "Dad", "Cameron", "Christian"]
+  family.unshift()
 end
 
 def remove_element_from_end_of_array(array)

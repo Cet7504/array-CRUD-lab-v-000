5bc9a905d4d8f668ab636566a714c4bec2824084
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-04-13 03:56:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  [Mom, Dad, Cameron, Christian]
+  []
 end
 
 def create_an_array
-
+  [Mom, Dad, Cameron, Christian]
 end
 
 def add_element_to_end_of_array(array, element)
